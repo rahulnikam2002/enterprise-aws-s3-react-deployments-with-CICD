15115bd48b7ab7547aa9cde77192c7268d137155
Rahul Nikam <codewithrahulnikam@gmail.com>
2026-06-25 23:14:39 +0530

    feat: add detailed documentation for formatCurrency function

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
+// This function formats a given number as currency in Indian Rupees (INR) using the `Intl.NumberFormat` API. It takes a number as input and returns a string representing the formatted currency value. The formatting style is set to "currency" and the currency is specified as "INR".
 export const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat("en-IN", {
     style: "currency",
